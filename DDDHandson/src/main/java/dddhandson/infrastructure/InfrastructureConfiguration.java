b383c159caf21fce75a3289b021cd94c25a728f4
rascioni <manuelrascioni@gmail.com>
2014-03-21 10:51:05 +0100

    Spring configuration + Controller

--- DDDHandson/src/main/java/dddhandson/infrastructure/InfrastructureConfiguration.java
+++ DDDHandson/src/main/java/dddhandson/infrastructure/InfrastructureConfiguration.java
@@ -1,29 +1,50 @@
 package dddhandson.infrastructure;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import dddhandson.domain.support.DomainRepository;
+import dddhandson.application.command.reclamo.ApriReclamoCommandHandler;
+import dddhandson.application.command.support.CommandDispatcher;
 import dddhandson.domain.support.EventPublisher;
 import dddhandson.domain.support.EventSourcingDomainRepository;
 import dddhandson.domain.support.EventStore;
+import dddhandson.infrastructure.commanddispatcher.MapCommandDispatcher;
 import dddhandson.infrastructure.eventpublisher.GuavaEventPublisher;
 import dddhandson.infrastructure.port.persistence.eventsourcing.adapter.memorydb.MemoryEventStore;
 
 @Configuration
 public class InfrastructureConfiguration {
+	
+	@Autowired
+	@Bean
+	public CommandDispatcher commandDispatcher(EventSourcingDomainRepository repository){
+		System.out.println("InfrastructureConfiguration.commandDispatcher()");
+		final MapCommandDispatcher commandDispatcher = new MapCommandDispatcher();
+		
+		commandDispatcher.register(new ApriReclamoCommandHandler(repository));
+		
+		return commandDispatcher;
+	}
 
 	
 	private EventPublisher eventPublisher(){
+//		return new GuavaEventPublisherFactory().createAsync();
 		return new GuavaEventPublisher();
 	}
 
-	private EventStore<String> eventStore(){
-		return new MemoryEventStore<String>(eventPublisher());
+	private EventStore eventStore(){
+		return new MemoryEventStore(eventPublisher());
 	}
 	
 	@Bean
-	public DomainRepository<String> domainRepository(){
+	public EventSourcingDomainRepository domainRepository(){
 		return new EventSourcingDomainRepository<>(eventStore());
+	}
+	
+	@Autowired
+	@Bean
+	public ApriReclamoCommandHandler apriReclamoCommandHandler(EventSourcingDomainRepository repository){
+		return new ApriReclamoCommandHandler(repository);
 	}
 }
--- DDDHandson/src/main/java/dddhandson/infrastructure/port/interfaces/adapter/rest/controller/TestController.java
+++ DDDHandson/src/main/java/dddhandson/infrastructure/port/interfaces/adapter/rest/controller/TestController.java
@@ -1,15 +1,29 @@
 package dddhandson.infrastructure.port.interfaces.adapter.rest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import dddhandson.application.command.reclamo.ApriReclamoCommand;
+import dddhandson.application.command.support.CommandDispatcher;
+
 @Controller
 public class TestController {
+	
+	private final CommandDispatcher commandDispatcher;
+	
+	
+	@Autowired
+    public TestController(CommandDispatcher commandDispatcher) {
+		this.commandDispatcher = commandDispatcher;
+	}
 
-    @RequestMapping("/test")
+	@RequestMapping("/test")
     @ResponseBody
-    public String test() {
+    public String test() throws Exception {
+		final ApriReclamoCommand command = new ApriReclamoCommand();
+		commandDispatcher.dispatch(command);
         return "asd";
     }
 
