264ce54d6a7e41c930dafb6ccf5ec4893321a138
rascioni <manuelrascioni@gmail.com>
2014-03-21 02:24:04 +0100

    removed dummy stuffs

--- DDDHandson/src/main/java/dddhandson/domain/model/DummyAggregateRepository.java
+++
@@ -1,6 +0,0 @@
-package dddhandson.domain.model;
-
-import dddhandson.domain.support.DomainRepository;
-
-public interface DummyAggregateRepository extends DomainRepository<DummyAggregate, String> {
-}
